1178be30899afc8245f182eb30bb5fa689c27126
Frank <frankwoord@live.nl>
2019-07-04 17:16:49 +0200

    Updates

---
+++ precache-manifest.80595081b768ad7d4c446572b46bff7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb14a405febe902d7668e380e90ffda",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "aea9765c17279ae658e3",
+    "url": "/calculator/static/css/main.84e604be.chunk.css"
+  },
+  {
+    "revision": "bdaeb9fbe66c99ff2cfa",
+    "url": "/calculator/static/js/2.c89b8069.chunk.js"
+  },
+  {
+    "revision": "aea9765c17279ae658e3",
+    "url": "/calculator/static/js/main.da69142f.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
